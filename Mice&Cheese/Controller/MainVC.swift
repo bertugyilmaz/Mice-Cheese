5ec55672fc5d26ec567337b7b3941d2d20cdbc96
Ben <onurhuseyincantay@onurs-MacBook-Pro.local>
2017-10-21 13:09:29 +0300

    Generating location for mouse and cheese controlling cells if it's a wall or not

--- Mice&Cheese/Controller/MainVC.swift
+++ Mice&Cheese/Controller/MainVC.swift
@@ -13,13 +13,16 @@ class MainVC: UIViewController{
     var isStart: Bool = false
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var startButton: RoundedButton!
-    
+    var mousePosition : Int!
+    var cheesePosition : Int!
+    var isValidPosition : Bool!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.configureCollectionView()
         
         self.startButton.addTarget(self, action: #selector(self.startButtonAction(_:)), for: .touchUpInside)
+        
     }
     
     override var prefersStatusBarHidden: Bool {
@@ -28,6 +31,23 @@ class MainVC: UIViewController{
     
     func startButtonAction(_ sender: UIButton) {
         self.isStart = !isStart
+        generateLocationForMouseAndCheese()
+    }
+    func generateLocationForMouseAndCheese() {
+        repeat{
+            isValidPosition = false
+            mousePosition = Int(arc4random_uniform(71))
+            cheesePosition = Int(arc4random_uniform(71))
+            let cell1 = self.collectionView!.cellForItem(at:  IndexPath(row: mousePosition,section: 0)) as! CollectionCell
+            let cell2 = self.collectionView!.cellForItem(at:  IndexPath(row: cheesePosition,section: 0)) as! CollectionCell
+            if cell1.isWall || cell2.isWall{
+                isValidPosition = false
+            }else{
+                isValidPosition = true
+            }
+            print("Onur : mouse \(mousePosition) cheese : \(cheesePosition)")
+            print("Onur : Cell1 isWall = \(cell1.isWall) Cell2 isWall = \(cell2.isWall)")
+        }while(mousePosition == cheesePosition && isValidPosition == true)
     }
     
     func configureCollectionView() {
@@ -63,12 +83,14 @@ extension MainVC : UICollectionViewDelegate,UICollectionViewDataSource {
         if !isStart {
             if let cell =  self.collectionView.cellForItem(at: indexPath) as? CollectionCell{
                 cell.backgroundView = UIImageView(image: UIImage(named: "wireImg"))
+                cell.isWall = true
             }
         }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionCell
+        cell.tag = indexPath.row + 1
         
         return cell
     }
